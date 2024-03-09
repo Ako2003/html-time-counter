40098fa7cd4ee718f5cf3a8ee2a4e7866cdd6cfc
Ako2003 <akifmursalov@gmail.com>
2024-03-09 12:17:17 -0800

    new

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const moment = require('moment');
+const path = require('path');
 
 const app = express();
 const port = 3000;
 
 
-app.use(express.static('files'));
+// Serve static files from the directory containing index.html
+app.use(express.static(path.join(__dirname)));
+
+// Define route to serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/files/index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server is listening at http://localhost:${port}`);
